f231bd839ed2cbcffed90469c4d658fa203dcb22
Maximiliano Traghetti <traghettimaximiliano64@gmail.com>
2025-09-25 11:53:53 -0300

    Se realizo el primer punto practico

---
+++ examen-de-ts.ts
@@ -0,0 +1,40 @@
+
+class InventarioViejo {
+    private items: { nombre: string }[] = [];
+
+    addItem(nombre: string): void {
+        this.items.push({ nombre: nombre });
+        console.log(`[ ${JSON.stringify(this.items[this.items.length - 1])} ]`); // Simplificado para mostrar solo el último añadido
+    }
+
+    getItems(): { nombre: string }[] {
+        return this.items;
+    }
+}
+
+
+interface NuevoInventario {
+    agregarEquipo(nombre: string, tipo: string, estado: string): void;
+}
+
+
+class AdaptadorInventario implements NuevoInventario {
+    private inventarioViejo: InventarioViejo;
+
+    constructor(inventarioViejo: InventarioViejo) {
+        this.inventarioViejo = inventarioViejo;
+    }
+
+    agregarEquipo(nombre: string, tipo: string, estado: string): void {
+  
+        this.inventarioViejo.addItem(nombre);
+    }
+}
+
+
+const viejoInventario = new InventarioViejo();
+const adaptador = new AdaptadorInventario(viejoInventario);
+
+
+adaptador.agregarEquipo("Router Cisco", "Red", "disponible");
+
